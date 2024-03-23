333b0e2ad46d6097e433aafda894fc571037f3d7
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-03-23 18:50:50 +0200

    added entity validation constant class

---
+++ BookStore/BookStore.Common/EntityValidationConstants.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Common
+{
+	public static class EntityValidationConstants
+	{
+		public class Author
+		{
+			public const int NameMinLength = 1;
+			public const int NameMaxLength = 30;
+
+			public const int DescriptionMinLength = 10;
+			public const int DescriptionMaxLength = 10000;
+		}
+
+		public class Book
+		{
+			public const int NameMinLength = 1;
+			public const int NameMaxLength = 50;
+
+			public const int DescriptionMinLength = 10;
+			public const int DescriptionMaxLength = 200;
+
+			public const string RatingMin = "1";
+			public const string RatingMax = "10";
+		}
+
+        public class  Character 
+        {
+			public const int NameMinLength = 1;
+			public const int NameMaxLength = 30;
+
+			public const int DescriptionMinLength = 10;
+			public const int DescriptionMaxLength = 5000;
+		}
+
+		public class Genre
+		{
+			public const int NameMinLength = 2;
+			public const int NameMaxLength = 20;
+		}
+
+        public class Publisher
+        {
+            
+        }
+    }
+}

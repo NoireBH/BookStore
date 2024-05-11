bc9771e29d78dcae934a6c68516da99feeff0edb
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-05-11 14:07:46 +0300

    added claimsPrincipalExtension

---
+++ BookStore/BookStore.Common/GeneralConstants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Common
+{
+    public class GeneralConstants
+    {
+        public class Page
+        {
+            public const int DefaultPage = 1;
+            public const int DefaultItemsPerPage = 6;
+        }
+
+        public class AdminUser
+        {
+            public const string AdminRoleName = "Administrator";
+            public const string AdminEmail = "admin_topmovies@gmail.com";
+            public const string AdminAreaName = "Admin";
+        }
+    }
+}
---
+++ BookStore/BookStore.Web.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+using static BookStore.Common.GeneralConstants.AdminUser;
+
+namespace BookStore.Web.Infrastructure.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string? GetId(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRoleName);
+        }
+    }
+}

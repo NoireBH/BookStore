1cf93b99930f1eca3bfca410e53ae79d5a04c425
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-05-22 19:21:14 +0300

    added checks for book details

--- BookStore/BookStore/Controllers/BookController.cs
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -1,5 +1,6 @@
 ﻿using BookStore.Controllers;
 using BookStore.Services.Data.Interfaces;
+using BookStore.Web.ViewModels.Books;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,7 +24,27 @@ namespace BookStore.Web.Controllers
 		[AllowAnonymous]
 		public async Task<IActionResult> Details(int id)
 		{
-			var movie = await bookService.GetBookDetailsByIdAsync(id);
+			bool movieExists = await bookService.ExistsById(id);
+
+			if (!movieExists)
+			{
+				//add toastr notification
+				return NotFound();
+			}
+
+			BookDetailsViewModel movie;
+
+			try
+			{
+				movie = await bookService.GetBookDetailsByIdAsync(id);
+			}
+			catch (Exception)
+			{
+				ModelState.AddModelError(string.Empty, "Unexpected error has occured, please try again...");
+				return StatusCode(500);
+			}
+
+
 
 			return View(movie);
 		}

98d0d6f2679d1328a8dcfcfa3e5db9e774ea29ed
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-05-27 15:36:18 +0300

    added functionality to buttons

--- BookStore/BookStore/wwwroot/js/site.js
+++ BookStore/BookStore/wwwroot/js/site.js
@@ -2,3 +2,39 @@
 function bookDescription() {
 
 }
+
+function decreaseAmount() {
+
+
+    $('#minus-btn').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var amount = document.getElementById("book-amount");
+
+        if (Number(amount.value) > 1) {
+            amount.value = Number(amount.value) - 1;
+        }
+
+    });
+
+    
+
+}
+
+function increaseAmount() {
+
+    $('#plus-btn').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var amount = document.getElementById("book-amount");
+
+        if (Number(amount.value) < 100) {
+            amount.value = Number(amount.value) + 1;
+        }
+
+    });
+
+    
+}

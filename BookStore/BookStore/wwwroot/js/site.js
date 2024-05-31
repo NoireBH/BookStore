8a803ac4bd134e13c3fe1416179afcd9d8319313
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-05-31 21:41:48 +0300

    changed links to buttons and hid some of the divs

--- BookStore/BookStore/wwwroot/js/site.js
+++ BookStore/BookStore/wwwroot/js/site.js
@@ -1,6 +1,42 @@
 ﻿
 function bookDescription() {
 
+    $('.book-details').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var bookDetails = document.getElementsByClassName("book-details")[0];
+
+        if (bookDetails.style.display == 'none') {
+            bookDetails.style.display == 'display-block'
+        }
+
+    });
+
+    $('.book-details-description').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var bookDetails = document.getElementsByClassName("book-details-description")[0];
+
+        if (bookDetails.style.display == 'none') {
+            bookDetails.style.display == 'display-block'
+        }
+
+    });
+
+    $('.book-details-authors').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var bookDetails = document.getElementsByClassName("book-details-authors")[0];
+
+        if (bookDetails.style.display == 'none') {
+            bookDetails.style.display == 'display-block'
+        }
+
+    });
+
 }
 
 function decreaseAmount() {
